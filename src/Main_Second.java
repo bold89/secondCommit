a1b1d3416ae12704877c580194404b28ca48e2bb
bold78 <100731980+bold78@users.noreply.github.com>
2022-03-07 06:21:06 +0200

    Second_commit

---
+++ src/Main_Second.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author tades
+ *
+ */
+public class Main_Second {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This Is Second Commit Iam Push");
+
+	}
+
+}
